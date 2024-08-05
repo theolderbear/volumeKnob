8a6249a8e1432689d498ffabbf29be010a2fe760
lucianip <paolo.luciani@tipico.com>
2024-08-05 20:59:27 +0200

    Battery code refactoring

--- VolumeKnob.ino
+++ VolumeKnob.ino
@@ -8,7 +8,7 @@ const long RIGHT_LEFT_DELAY = 100;
 const long BLINK_DELAY = 250;
 const long LONG_PRSS_DELAY = 1000;
 const long PARING_TIME = 10000;
-const long BATTERY_LEVEL_TIME = 3000;  // How often to check battery level
+const long BATTERY_LEVEL_TIME = 15 * 60 * 1000;  // How often to check battery level
 const long CRITICAL_BATTERY_LEVEL = 10;
 const long LOW_BATTERY_LEVEL = 30;
 const long BATTERY_DELAY = 2000;  // How long the low battery alert should be held
@@ -19,9 +19,10 @@ const int greenPin = 6;
 const int bluPin = 7;
 const int batteryPin = A0;
 
-const float MAX_BATTERY_V = 5.5;
+const float MAX_BATTERY_V = 6.20;
 const float MAX_BATTERY_V_DELTA = 4;
-const float MAX_PIN_VOLTAGE = 3.3; // Max voltage that can be read from a pin
+const float MAX_PIN_VOLTAGE = 3.3;  // Max voltage that can be read from a pin
+
 const unsigned int MAX_ANALOG_READ = 4095;
 const unsigned int R1_V_DIVIDER = 5000;
 const unsigned int R2_V_DIVIDER = 5000;
@@ -32,6 +33,7 @@ long previousPos = 0;
 bool pressedRotation = false;
 bool ignoreSingleClick = false;
 bool paringRunnig = false;
+int batteryLevelTest = -1; // Set to 0 to mock the battery
 
 enum Mode {
   VOLUME_SCREEN = 0,
@@ -108,22 +110,23 @@ void setBatteryLevel() {
 }
 
 int getBatteryPercentage() {
-  // int batteryLevel = 40 - ct * 10;
+  
+  if (batteryLevelTest != -1) {
+    batteryLevelTest = batteryLevelTest + 1;
+    int batteryLevel = 40 - batteryLevelTest * 10;
+    if (batteryLevel < 0) {
+      batteryLevelTest = 0;
+    }
+    return batteryLevelTest;
+  }
+
   int dividePin = analogRead(batteryPin);
   float divideV = dividePin * MAX_PIN_VOLTAGE / MAX_ANALOG_READ;
   float v = divideV * (R1_V_DIVIDER + R2_V_DIVIDER) / R2_V_DIVIDER;
   int batteryLevel = 100 / (MAX_BATTERY_V - MAX_BATTERY_V_DELTA) * (v - MAX_BATTERY_V_DELTA);
-  // ct = ct + 1;
 
-  Serial.print("battery pin : ");
-  Serial.print(dividePin);
-  Serial.print(", dv : ");
-  Serial.print(divideV);
-  Serial.print(", v : ");
-  Serial.print(v);
-  Serial.print(", % : ");
-  Serial.println(batteryLevel);
 
+  printBatteryLevel(dividePin, divideV, v, batteryLevel);
   return batteryLevel;
 }
 
@@ -225,4 +228,15 @@ void checkRotation() {
 
 bool encoderButtonPressed() {
   return !digitalRead(buttonPin);
+}
+
+void printBatteryLevel(int dividePin, float divideV, float v, int batteryLevel) {
+  Serial.print("Battery, pin : ");
+  Serial.print(dividePin);
+  Serial.print(", dv : ");
+  Serial.print(divideV);
+  Serial.print(", v : ");
+  Serial.print(v);
+  Serial.print(", % : ");
+  Serial.println(batteryLevel);
 }
